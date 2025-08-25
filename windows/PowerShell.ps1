6fd9754eaa1976af5afd96fb474d60b0f44df767
Kirill Solovey <kirasolovey09@gmail.com>
2025-08-25 15:10:31 +0500

    update pwsh profile

--- windows/PowerShell.ps1
+++ windows/PowerShell.ps1
@@ -20,11 +20,10 @@ if (Get-Command oh-my-posh -ErrorAction Ignore) {
     Set-Theme night-owl
 }
 
-
 ### Modules
 Import-module -Name Terminal-Icons
-Import-Module -Name Microsoft.WinGet.CommandNotFound
-Import-Module -Name PowerShellProTools
+# Import-Module -Name Microsoft.WinGet.CommandNotFound
+# Import-Module -Name PowerShellProTools
 
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
@@ -37,12 +36,14 @@ Set-PSReadLineOption -PredictionSource History
 
 
 ### Completions
-(& uv generate-shell-completion powershell) | Out-String | Invoke-Expression
-(& uvx --generate-shell-completion powershell) | Out-String | Invoke-Expression
+if (Get-Command uv -ErrorAction Ignore) {
+    (& uv generate-shell-completion powershell) | Out-String | Invoke-Expression
+    (& uvx --generate-shell-completion powershell) | Out-String | Invoke-Expression
+}
 
 
 ### Aliases
-Set-Alias -Name touch -Value 'New-Item'
+Set-Alias -Name touch -Value New-Item
 
 function .. { Set-Location -Path .. }
 function ... { Set-Location -Path ..\.. }
@@ -58,7 +59,7 @@ Set-PSReadlineKeyHandler -Key ctrl+d -Function ViExit
 
 ### Utils
 
-# Yazi
+## Yazi
 if (Get-Command yazi.exe -ErrorAction Ignore) {
     function yy {
         $tmp = [System.IO.Path]::GetTempFileName()
@@ -71,7 +72,7 @@ if (Get-Command yazi.exe -ErrorAction Ignore) {
     }
 }
 
-# Eza
+## Eza
 if (Get-Command eza.exe -ErrorAction Ignore) {
     $EZA_DEFAULT_OPTS = (
         '--git',
@@ -91,65 +92,62 @@ if (Get-Command eza.exe -ErrorAction Ignore) {
     function ListItems { eza.exe $EZA_DEFAULT_OPTS @args }
     Set-Alias -Name ls -Value ListItems
 
-    function tree { eza.exe $EZA_DEFAULT_OPTS --tree @args }
+    function tree { ls --tree @args }
 
-    function la { eza.exe $EZA_DEFAULT_OPTS --sort=Name --all @args }
-    function l { eza.exe $EZA_DEFAULT_OPTS --header --long @args }
-    function ll { eza.exe $EZA_DEFAULT_OPTS --all --header --long @args }
+    function la { ls --sort=Name --all @args }
+    function l { ls --header --long @args }
+    function ll { l --all @args }
 
     # Full information about files
-    function lla { eza.exe $EZA_DEFAULT_OPTS -lbhHigUmuSa @args }
-    function llx { eza.exe $EZA_DEFAULT_OPTS -lbhHigUmuSa@ @args }
+    function lla { ls -lbhHigUmuSa @args }
+    function llx { ls -lbhHigUmuSa@ @args }
 
     # Ls with sorting
-    function lgit { eza.exe $EZA_DEFAULT_OPTS --all --git-ignore @args }
-    function lmod { eza.exe $EZA_DEFAULT_OPTS --all --header --long --sort=modified @args }
-    function lcreate { eza.exe $EZA_DEFAULT_OPTS --all --header --long --sort=created @args }
-    function lsize { eza.exe $EZA_DEFAULT_OPTS --all --header --long --sort=size @args }
-    function ldirs { eza.exe $EZA_DEFAULT_OPTS --only-dirs @args }
-    function lfiles { eza.exe $EZA_DEFAULT_OPTS --only-files @args }
+    function lgit { ls --all --git-ignore @args }
+    function lmod { ls --all --header --long --sort=modified @args }
+    function lcreate { ll --sort=created @args }
+    function lsize { ll --sort=size @args }
+    function ldirs { ls --only-dirs @args }
+    function lfiles { ls --only-files @args }
 
     # Tree with level (first argument)
-    function lt {
-        $level = if ($args.Count -eq 0) { "1" } else { $args[0] }
-        $args = if ($args.Count -le 1) { "." } else { $args[1..($args.Count-1)] }
-
+    function lt ($level = 1) {
         tree --level=$level @args
     }
 
-    function ls1 { eza.exe $EZA_DEFAULT_OPTS --tree --level=1 @args }
-    function ls2 { eza.exe $EZA_DEFAULT_OPTS --tree --level=2 @args }
+    function ls1 { ls --tree --level=1 @args }
+    function ls2 { ls --tree --level=2 @args }
 
     # Others
-    function lT { eza.exe $EZA_DEFAULT_OPTS --tree --no-user --all --ignore-glob=$EZA_IGNORE_GLOB @args }
-    function lS { eza.exe $EZA_DEFAULT_OPTS --oneline @args }
-    function lX { eza.exe $EZA_DEFAULT_OPTS --across @args }
-    function lR { eza.exe $EZA_DEFAULT_OPTS --recurse @args }
+    function ltt { tree --no-user --all --ignore-glob=$EZA_IGNORE_GLOB @args }
+    function lss { ls --oneline @args }
+    function lxx { ls --across @args }
+    function lrr { ls --recurse @args }
 
-    function labs { eza.exe $EZA_DEFAULT_OPTS --absolute=on @args }
-    function lpwd { eza.exe $EZA_DEFAULT_OPTS --absolute=on -d . @args }
+    function labs { ls --absolute=on @args }
+    function lpwd { labs -d . @args }
 }
 
-# Fastfetch
+## Fastfetch
 if (Get-Command fastfetch.exe -ErrorAction Ignore) {
-    function ff { fastfetch.exe --load-config examples/17.jsonc $args }
-    function fff { fastfetch.exe --load-config examples/13.jsonc $args --logo none }
+    function ff { fastfetch.exe --load-config examples/17.jsonc @args }
+    function fff { fastfetch.exe --load-config examples/13.jsonc @args --logo none }
 
-    function paleofetch { fastfetch.exe --load-config paleofetch.jsonc $args }
-    function neofetch { fastfetch.exe --load-config neofetch.jsonc $args }
-    function archey { fastfetch.exe --load-config archey.jsonc --logo arch2 $args }
+    function paleofetch { fastfetch.exe --load-config paleofetch.jsonc @args }
+    function neofetch { fastfetch.exe --load-config neofetch.jsonc @args }
+    function archey { fastfetch.exe --load-config archey.jsonc @args }
 
     function ffc {
         param ( $number_config = 13 )
-        fastfetch.exe --load-config examples/$number_config.jsonc $args
+        fastfetch.exe --load-config examples/$number_config.jsonc @args
     }
 }
 
-# Git
+## Git
 if (Get-Command git.exe -ErrorAction Ignore) {
     Set-Alias -Name g -Value git
-    function gst { git.exe status $args }
-    function glog { git.exe log --oneline --decorate --graph $args }
+    function gst { git.exe status @args }
+    function glog { git.exe log --oneline --decorate --graph @args }
 }
 
 
