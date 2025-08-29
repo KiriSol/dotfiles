8ae48d616b79596be23fa52826e0c739cfaa4307
Kirill Solovey <kirasolovey09@gmail.com>
2025-08-29 23:11:40 +0500

    format ps1 scripts

--- windows/PowerShell.ps1
+++ windows/PowerShell.ps1
@@ -68,12 +68,12 @@ if (Get-Command -Name scoop.ps1 -ErrorAction Ignore) {
 ## Winget
 Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
     param ($wordToComplete, $commandAst, $cursorPosition)
-        [Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
-        $Local:word = $wordToComplete.Replace('"', '""')
-        $Local:ast = $commandAst.ToString().Replace('"', '""')
-        winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition | ForEach-Object {
-            [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
-        }
+    [Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
+    $Local:word = $wordToComplete.Replace('"', '""')
+    $Local:ast = $commandAst.ToString().Replace('"', '""')
+    winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition | ForEach-Object {
+        [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+    }
 }
 
 ## Uv (python package and environment manager)
