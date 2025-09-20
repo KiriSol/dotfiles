8d2dbcf13914c181e89c9311b0159b19ab2ab45b
Kirill Solovey <kirasolovey09@gmail.com>
2025-09-20 13:51:19 +0500

    add auto install

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/env sh
+
+set -e
+
+# Check dotbot binary
+if command -v dotbot >/dev/null; then
+    DOTBOT_BIN="dotbot"
+else
+    echo "dotbot not found"
+    return 1
+fi
+
+CONFIG=".install.conf.yaml"
+
+BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd "${BASEDIR}"
+
+# Run
+"${DOTBOT_BIN}" -d "${BASEDIR}" -c "${CONFIG}" "${@}"
+
+# Termux
+if [ -n "$TERMUX_VERSION" ]; then
+    cp termux/shortcuts ~/.shortcuts -r --no-target-directory
+    cp termux/termux/boot ~/.termux/boot -r --no-target-directory
+    ln -s "$SHELL" ~/.termux/shell -f
+fi
