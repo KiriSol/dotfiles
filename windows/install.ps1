3a2cb8c44b942bcab3bc0dad693e3ad513854193
Kirill Solovey <kirasolovey09@gmail.com>
2025-10-30 10:34:13 +0500

    add winterm to auto install

--- windows/install.ps1
+++ windows/install.ps1
@@ -18,3 +18,7 @@ Set-Location $BASEDIR
 
 # Run
 & $DOTBOT_BIN.Source -d "$BASEDIR\.." -c $CONFIG $args
+
+if ((Get-Content "$BASEDIR/apps/Microsoft.WindowsTerminal.Preview/settings.json").Count -gt (Get-Content "$HOME\AppData\Local\Packages\Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe\LocalState\settings.json").Count) {
+    Copy-Item "$BASEDIR/apps/Microsoft.WindowsTerminal.Preview/setting.json" "$HOME\AppData\Local\Packages\Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe\LocalState\settings.json"
+}
