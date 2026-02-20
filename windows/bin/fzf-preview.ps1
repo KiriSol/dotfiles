9b8383ac615f8b4d8dbe4fd4f97d44c9264563ab
Kirill Solovey <kirasolovey09@gmail.com>
2026-02-20 08:51:10 +0500

    clean fzf-preview.ps1

--- windows/bin/fzf-preview.ps1
+++ windows/bin/fzf-preview.ps1
@@ -7,150 +7,83 @@ if (-not (Test-Path -Path $Path)) {
     exit 1
 }
 
-function Get-MimeType {
-    param($FileName)
-    if (Get-Command -Name file -ErrorAction SilentlyContinue) {
-        return "$(file --brief --dereference --mime-type -- $FileName)"
-    }
-    if ($(Get-Item -Path $FileName).PSIsContainer) {
-        return 'inode/directory'
-    }
-    try {
-        # Cache DB for best performance
-        if (-not $global:MimeDbCache) {
-            $Global:MimeDbCache = Invoke-RestMethod -Uri "https://cdn.jsdelivr.net/gh/jshttp/mime-db@master/db.json"
-        }
-        $MediaTypes = $Global:MimeDbCache.psobject.Properties
-        $MatchingMediaType = $mediaTypes.Where(
-            { $_.Value.extensions -contains [IO.Path]::GetExtension($FileName).Substring(1) },
-            'First'
-        ).Name
-        # Use a fallback type, if no match was found.
-        if (-not $MatchingMediaType) { $MatchingMediaType = 'application/octet-stream' }
-
-        return $MatchingMediaType
-    }
-    catch {
-        # Fallback
-        $ext = [System.IO.Path]::GetExtension($File).ToLower()
-        switch ($ext) {
-            '.txt' { return 'text/plain' }
-            '.md' { return 'text/markdown' }
-            '.json' { return 'application/json' }
-            '.xml' { return 'application/xml' }
-            '.ps1' { return 'text/x-powershell' }
-            '.cmd' { return 'text/x-msdos-batch' }
-            '.c' { return 'text/x-c' }
-            '.cpp' { return 'text/x-cpp' }
-            '.py' { return 'text/plain' }
-            '.js' { return 'text/plain' }
-            '.jpg' { return 'image/jpeg' }
-            '.jpeg' { return 'image/jpeg' }
-            '.png' { return 'image/png' }
-            '.gif' { return 'image/gif' }
-            '.mp4' { return 'video/mp4' }
-            '.avi' { return 'video/x-msvideo' }
-            '.zip' { return 'application/zip' }
-            '.7z' { return 'application/x-7z-compressed' }
-            default { return 'application/octet-stream' }
-        }
-    }
-}
-
-
 try {
     $item = Get-Item -Path $Path -ErrorAction Stop
-}
-catch {
-    Write-Host "Permission denied: $Path"
+} catch {
+    Write-Host "❌ Permission denied: $Path"
     exit 1
 }
 
 if ($item.PSIsContainer) {
-    Write-Host "📁 '$($item.Name)' directory content:"
-    if (Get-Command -Name eza -ErrorAction SilentlyContinue) {
+    Write-Host "📁 Directory:"
+    if (Get-Command -Name eza -ErrorAction Ignore) {
         eza --icons=always --color=always --header --long --no-time --no-permissions -- $Path
-    }
-    else {
+    } else {
         Get-ChildItem -Path $Path -Force | Format-Table -AutoSize Name, Length, LastWriteTime
     }
     exit 0
 }
 
-$mimeType = Get-MimeType $Path
-
-if ($mimeType -eq 'inode/directory') {
-    Write-Host "📁 '$($item.Name)' directory content:"
-    if (Get-Command -Name eza -ErrorAction SilentlyContinue) {
-        eza --icons=always --color=always --header --long --no-time --no-permissions -- $Path
-    }
-    else {
-        Get-ChildItem -Path $Path -Force | Format-Table -AutoSize Name, Length, LastWriteTime
-    }
-    exit 0
-}
-
-if ($Host.UI.RawUI.WindowSize.Height -gt 10) {
-    Write-Host "📊 Information:"
-    Write-Host "    Name: '$($item.Name)'"
-    Write-Host "    Size: $([math]::Round($item.Length/1KB, 2)) KB"
-    Write-Host "    MIME-type: $mimeType"
-    Write-Host "🎯 Content:"
+if (Get-Command -Name file -ErrorAction Ignore) {
+    $mimeType = "$(file --brief --dereference --mime-type -- $Path)"
+} else {
+    Write-Host "❌ Do you have 'GNU file' installed?"
+    exit 1
 }
 
 if ($mimeType -eq 'inode/x-empty') {
-    Write-Host "Empty file"
+    Write-Host "Empty file" -BackgroundColor White -ForegroundColor Black
+    exit 0
 }
-elseif ($mimeType -like 'text/*' -or $mimeType -like 'application/json*' -or $mimeType -like 'application/xml*') {
-    if (Get-Command -Name bat -ErrorAction SilentlyContinue) {
+
+if ($Host.UI.RawUI.WindowSize.Height -gt 5) {
+    Write-Host "📊 Information:"
+    Write-Host " - Name: '$($item.Name)'"
+    Write-Host " - Size: $([math]::Round($item.Length/1KB, 2)) KB"
+    Write-Host " - MIME-type: $mimeType"
+    Write-Host "🎯 Content:"
+}
+
+if ($mimeType -like 'text/*' -or $mimeType -like 'application/json*' -or $mimeType -like 'application/xml*') {
+    if (Get-Command -Name bat -ErrorAction Ignore) {
         bat --style=numbers --color=always --pager=never --highlight-line=0 -- "$Path"
-    }
-    else {
+    } else {
         Get-Content -Path $Path -TotalCount 50 | ForEach-Object {
             $line = $_
             if ($line.Length -gt 120) {
                 $line.Substring(0, 117) + "..."
-            }
-            else {
+            } else {
                 $line
             }
         }
     }
-}
-elseif ($mimeType -like 'image/*' -or $mimeType -like 'video/*') {
-    if (Get-Command -Name chafa -ErrorAction SilentlyContinue) {
+    Write-Host "..."
+} elseif ($mimeType -like 'image/*' -or $mimeType -like 'video/*') {
+    if (Get-Command -Name chafa -ErrorAction Ignore) {
         chafa --size=$(
             if ($Host.UI.RawUI.WindowSize.Width)
                 { $Host.UI.RawUI.WindowSize.Width - 3 }
             else
                 { 60 }
         ) "$Path"
+    } else {
+        Write-Host "❌ Do you have 'chafa' installed?"
+        if (Get-Command -Name file -Ignore) { file $Path }
     }
-    else {
-        Write-Host "🖼️ Media-file, please install chafa"
-        if (Get-Command -Name file -SilentlyContinue) { file $Path }
-    }
-}
-elseif ($mimeType -like 'application/*zip*' -or $mimeType -like 'application/*rar*') {
-    if (Get-Command -Name 7z -ErrorAction SilentlyContinue) {
+} elseif ($mimeType -like 'application/*zip*' -or $mimeType -like 'application/*rar*') {
+    if (Get-Command -Name 7z -ErrorAction Ignore) {
         Write-Host "📦 Archive content:"
         7z l "$Path" | Select-Object -First 50
+        Write-Host "..."
+    } else {
+        Write-Host "❌ Do you have '7zip' installed?"
+        if (Get-Command -Name file -Ignore) { file $Path }
     }
-    else {
-        Write-Host "📦 Archive, please install 7zip"
-    }
-}
-else {
-    if (Get-Command -Name file -ErrorAction SilentlyContinue) {
-        file $Path
-    }
-    Write-Host "🔢 HEX:"
-    if (Get-Command -Name strings -ErrorAction SilentlyContinue) {
+} else {
+    if (Get-Command -Name file -ErrorAction Ignore) { file $Path }
+    if (Get-Command -Name strings -ErrorAction Ignore) {
+        Write-Host "🔢 HEX:"
         strings $Path | Select-Object -First 50
-    }
-    else {
-        $bytes = [System.IO.File]::ReadAllBytes($Path)
-        $hex = [System.BitConverter]::ToString($bytes[0..15]) -replace '-', ' '
-        Write-Host "$hex"
+        Write-Host "..."
     }
 }
