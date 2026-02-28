e0de0aa05e03e77996186d15b1d6387230f7f48a
Kirill Solovey <kirasolovey09@gmail.com>
2026-02-28 14:14:14 +0500

    add auto install font for termux

--- install.sh
+++ install.sh
@@ -23,4 +23,5 @@ if [ -n "$TERMUX_VERSION" ]; then
     rm -rf ~/.shortcuts
     cp termux/shortcuts ~/.shortcuts -r
     ln -s "$SHELL" ~/.termux/shell -f
+    curl -Lo ~/.termux/font.ttf https://raw.githubusercontent.com/ryanoasis/nerd-fonts/raw/refs/heads/master/patched-fonts/JetBrainsMono/Ligatures/Regular/JetBrainsMonoNerdFont-Regular.ttf
 fi
