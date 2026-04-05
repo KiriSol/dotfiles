b280147f084e96a61dab4876b39766a1c4904c91
Kirill Solovey <kirasolovey09@gmail.com>
2026-04-05 21:23:09 +0500

    rename: yy -> y

--- config/fish/functions/y.fish
+++ config/fish/functions/y.fish
@@ -1,5 +1,5 @@
 if type -q yazi
-    function yy --description "Yazi with cwd sync"
+    function y --description "Yazi with cwd sync"
         set tmp (mktemp -t "yazi-cwd.XXXXXX")
         command yazi $argv --cwd-file="$tmp"
         if read -z cwd < "$tmp"; and [ "$cwd" != "$PWD" ]; and test -d "$cwd"
